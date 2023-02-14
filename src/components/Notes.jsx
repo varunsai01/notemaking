122e9d117e53ae276f5d62ee27967f31e096aeae
Vemulawada varunsai <109099106+varunsai01@users.noreply.github.com>
2023-02-14 17:21:27 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Today plan</h1>
+      <p>
+        Complete social science roam with friends eat outside do code and sleep
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
